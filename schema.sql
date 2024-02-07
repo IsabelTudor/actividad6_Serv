00b5b41a79ffa919fbc87a2516193a88c6e7759a
IsisTD <220126@fppiramide.com>
2024-02-07 11:00:26 +0100

    update

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE usuarios (
     id serial primary key,
     nombre varchar(100) unique,
     password varchar(255)
-);
+); 
 CREATE TABLE videojuegos (
     id serial primary key,
     nombre varchar(255)
